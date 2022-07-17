d88d238beebd2e6229b2a66c493cceb25fdc2f20
Alexis Parave <aparavev@gmail.com>
2022-07-16 22:39:52 -0500

    FEAT: this is a simple app to upload an image to my account in cloudinary

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+  //This is the url of the api for my account in cloudinary
+  const url = "https://api.cloudinary.com/v1_1/alexisparave/image/upload";
+  const [imageSelected, setImageSelect] = useState("")
+
+  function handleChange(e){
+    setImageSelect(e.target.files[0])
+  }
+
+  function handlesubmit(e){
+    e.preventDefault()
+    // A form data is created because this type of structure is send in  a post request to upload and image in cloudinary 
+    const formData = new FormData();
+    formData.append("file", imageSelected)
+    // This are my credentials we need to search a way to encrypt or use a environment variable
+    formData.append("upload_preset", "ywcdj6mz")
+
+    // Post request to upload an image in cloudinary
+    fetch(url, {
+      method: "POST",
+      body: formData
+    })
+    .then((res)=> res.json())
+    .then((image_data)=> console.log(image_data))
+    .then(console.log("uploaded"))
+
+    // From image_data we need image_data.secure_url to add to our company model so we need to add another field to our company model for image_url
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Tets cloudinary</h1>
+      <form onSubmit={handlesubmit}>
+        <input 
+          type ="file" 
+          name="image" 
+          onChange={handleChange}
+        />
+        <br/>
+        <button type = "submit" >Upload</button><br/>
+      </form>
+    </>
   );
 }
 
